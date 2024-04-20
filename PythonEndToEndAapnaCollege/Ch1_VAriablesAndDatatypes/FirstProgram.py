4485bc8d8ff50260f91a7409e59ad5a37cb0f225
saifrahmania <89687720+saifrahmania@users.noreply.github.com>
2024-04-20 21:20:02 +0600

    experiment

---
+++ PythonEndToEndAapnaCollege/Ch1_VAriablesAndDatatypes/FirstProgram.py
@@ -0,0 +1,90 @@
+from math import floor
+print("hello world")
+print("This is Saifur Rahman from Bangladesh\n I have completed my graduation from EWU\n I am a software engineer\n I am learning python from Aapna College\n I want to learn python\n the main reson of learning python is to stdy datascience and machine learning in the future\n I am very excited")
+print("""here i am going to write an entire paragraph in this paragraph i will write about my self and my future plan. I am Saifur Rahman from Bangladesh. I have completed my graduation from East West University. I am a software engineer. I am learning python from Aapna College. I want to learn python. The main reason of learning python is to study data science and machine learning in the future. I am very excited.""")
+
+print(5+3)
+print(5-3)
+print(5*3)
+print(5/3)
+print(5//3)
+print(5%3)
+
+name = "Saifur Rahman"
+age = 25
+university = "East West University"
+print(name)
+print(age)
+print(university)
+name = "ibnSina"
+print(name)
+price = 50
+print(price)
+price = 100
+print(price)
+price2 = price
+print("the value of price 2: ",price2)
+
+while name is "Mushfiqur Rahman":
+    print("This is Mushfiqur Rahman")
+    break
+else:
+    print("you are not the person i am looking for")
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(price2))
+print(type(university))
+boolean = True
+print(type(boolean))
+
+if boolean is True:
+    print("the name has been changed")
+else:
+    print("the name has not been changed")
+
+
+a = 5
+b = 3
+sum = a+b
+difference = a-b
+print("Sum of A and B: ",sum)
+print("Difference of A and B: ",difference)
+
+# Expression Execution
+
+a,b = 5,3
+sum = a+b
+print("expression execution: ")
+difference = a-b
+print("Sum of A and B: ",sum)
+print("Difference of A and B: ",difference)
+print("Flore value of A/B: ",floor(a/b))
+
+Txt = "@"
+print(4*Txt*7)
+
+# Concatenation
+first_name = "Saifur"
+last_name = "Rahman"
+full_name = first_name + " " + last_name
+print(full_name)
+
+first_var = "Saifur"
+second_var = "2"
+third_var = 3
+
+print((first_var + second_var)*third_var)
+
+# remainder 
+
+a =  - 5
+b =  3
+c = a%b
+print(c)
+
+"""
+comments are written here আমি এখানে কমেন্ট লিখছি
+"""
+
