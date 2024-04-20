c5d815013ed8ca907f87ec94f5086524235a09a0
saifrahmania <89687720+saifrahmania@users.noreply.github.com>
2024-04-20 22:51:24 +0600

    code practiced until 1:39:42

---
+++ PythonEndToEndAapnaCollege/Ch1_VAriablesAndDatatypes/CSTutorial.py
@@ -0,0 +1,52 @@
+a = 5
+b = 3
+sum = a+b
+difference = a-b
+print("Sum of A and B: ",sum)
+print("Difference of A and B: ",difference)
+
+# Expression Execution
+
+a,b = 5,3
+sum = a+b
+print("expression execution: ")
+difference = a-b
+print("Sum of A and B: ",sum)
+print("Difference of A and B: ",difference)
+print("Flore value of A/B: ",floor(a/b))
+
+Txt = "@"
+print(4*Txt*7)
+
+# Concatenation
+first_name = "Saifur"
+last_name = "Rahman"
+full_name = first_name + " " + last_name
+print(full_name)
+
+first_var = "Saifur"
+second_var = "2"
+third_var = 3
+
+print((first_var + second_var)*third_var)
+
+# remainder 
+
+a =  - 5
+b =  3
+c = a%b
+print(c)
+
+"""
+comments are written here আমি এখানে কমেন্ট লিখছি
+"""
+
+# providing input 
+name = input("Enter your name: ")
+print("Your name is: ",name)
+age = int(input("Enter your age: "))
+print("Your age is: ",age)
+university = input("Enter your university name: ")
+print("Your university is: ",university)
+
+
