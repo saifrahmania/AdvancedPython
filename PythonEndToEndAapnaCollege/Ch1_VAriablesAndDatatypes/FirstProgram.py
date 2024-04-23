4b391e9b0b4d4642f50493d87ed1117f6d3e7f0c
saifrahmania <89687720+saifrahmania@users.noreply.github.com>
2024-04-23 08:52:32 +0600

    practice completed: 2 hours 20 minutes 12 seconds

--- PythonEndToEndAapnaCollege/Ch1_VAriablesAndDatatypes/FirstProgram.py
+++ PythonEndToEndAapnaCollege/Ch1_VAriablesAndDatatypes/FirstProgram.py
@@ -1,47 +1,176 @@
-from math import floor
-print("hello world")
-print("This is Saifur Rahman from Bangladesh\n I have completed my graduation from EWU\n I am a software engineer\n I am learning python from Aapna College\n I want to learn python\n the main reson of learning python is to stdy datascience and machine learning in the future\n I am very excited")
-print("""here i am going to write an entire paragraph in this paragraph i will write about my self and my future plan. I am Saifur Rahman from Bangladesh. I have completed my graduation from East West University. I am a software engineer. I am learning python from Aapna College. I want to learn python. The main reason of learning python is to study data science and machine learning in the future. I am very excited.""")
+# from math import floor
+# print("hello world")
+# print("This is Saifur Rahman from Bangladesh\n I have completed my graduation from EWU\n I am a software engineer\n I am learning python from Aapna College\n I want to learn python\n the main reson of learning python is to stdy datascience and machine learning in the future\n I am very excited")
+# print("""here i am going to write an entire paragraph in this paragraph i will write about my self and my future plan. I am Saifur Rahman from Bangladesh. I have completed my graduation from East West University. I am a software engineer. I am learning python from Aapna College. I want to learn python. The main reason of learning python is to study data science and machine learning in the future. I am very excited.""")
 
-print(5+3)
-print(5-3)
-print(5*3)
-print(5/3)
-print(5//3)
-print(5%3)
+# print(5+3)
+# print(5-3)
+# print(5*3)
+# print(5/3)
+# print(5//3)
+# print(5%3)
+
+# name = "Saifur Rahman"
+# age = 25
+# university = "East West University"
+# print(name)
+# print(age)
+# print(university)
+# name = "ibnSina"
+# print(name)
+# price = 50
+# print(price)
+# price = 100
+# print(price)
+# price2 = price
+# print("the value of price 2: ",price2)
+
+# while name is "Mushfiqur Rahman":
+#     print("This is Mushfiqur Rahman")
+#     break
+# else:
+#     print("you are not the person i am looking for")
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# print(type(price2))
+# print(type(university))
+# boolean = True
+# print(type(boolean))
+
+# if boolean is True:
+#     print("the name has been changed")
+# else:
+#     print("the name has not been changed")
+
+# # arithmatic operators
+# a = 5
+# b = 3
+# c = 3
+# d = 7
+# sum = a+b
+# difference = a-b
+# print("Sum of A and B: ",sum)
+# print("Difference of A and B: ",difference)
+# mul = a*b
+# div = a/b
+# print("Multiplication of A and B: ",mul)
+# print("Division of A and B: ",div)
+# floor_div = a//b
+# print("Floor Division of A and B: ",floor_div)
+# remainder = a%b
+# print("Remainder of A and B: ",remainder)
+# power = a**b
+# print("Power of A and B: ",power)
+
+# # relational operators
+# logical_explanation = "A is greater than B" if a>b else "B is greater than A"
+# print(logical_explanation)
+# logical_explanation2 = "B and C are equal" if b==c else "B and C are not equal"
+# print(logical_explanation2)
+# logical_expression3 = "A is not equal to D" if a!=d else "A is equal to D"
+# print(logical_expression3)
+# logical_explanation4 = "A is less than D" if a<d else "A is greater than D"
+# print(logical_explanation4)
+
+# # Assignment operators
+
+# a = 5
+# print("Old value of A: ",a)
+# a += 3
+# print("New value of A: ",a)
+# a -= 4
+# print("Another new value of A: ",a)
+# a *= 3
+# print("Another second new value of A: ",a)
+# a /= 2
+# print("Another third new value of A: ",a)
+
+# # Logical operators
+
+# a = 5
+# b = 3
+
+# print("Logical AND: ",a>b and a<b)
+# print("Logical OR: ",a>b or a<b)
+# print("Logical NOT: ",not a>b)
+# print("Logical NOT: ",not a<b)
+
+# Bitwise operators
+
+a = 5
+b = 3
+print("Binary of A: ",bin(a))
+print("Binary of B: ",bin(b))
+print("Bitwise AND: ",a&b)
+print("Bitwise OR: ",a|b)
+print("Bitwise XOR: ",a^b)
+print("Bitwise NOT: ",~a)
+print("Bitwise Left Shift: ",a<<b)
+print("Bitwise Right Shift: ",a>>b)
+
+# Identity operators
+
+a = 5
+b = 3
+
+print("Identity is: ",a is b)
+print("Identity is not: ",a is not b)
+
+# Membership operators
 
 name = "Saifur Rahman"
-age = 25
-university = "East West University"
-print(name)
-print(age)
-print(university)
-name = "ibnSina"
-print(name)
-price = 50
-print(price)
-price = 100
-print(price)
-price2 = price
-print("the value of price 2: ",price2)
+print("Saifur" in name)
+print("Rahman" not in name)
 
-while name is "Mushfiqur Rahman":
-    print("This is Mushfiqur Rahman")
-    break
-else:
-    print("you are not the person i am looking for")
+# Type conversion
 
-print(type(name))
-print(type(age))
-print(type(price))
-print(type(price2))
-print(type(university))
-boolean = True
-print(type(boolean))
+a = 5
+b = 3
+c = 3.5
+d = "5"
+e = "3"
+f = "3.5"
+g = True
+h = False
+i = 0
+j = 1
+k = 0.0
+l = 1.0
+m = "True"
+n = "False"
+o = "0"
+p = "1"
+q = "0.0"
+r = "1.0"
+print("Type of A: ",type(a))
+print("Type of B: ",type(b))
+print("Type of C: ",type(c))
+print("Type of D: ",type(d))
+print("Type of E: ",type(e))
+print("Type of F: ",type(f))
+print("Type of G: ",type(g))
+print("Type of H: ",type(h))
+print("Type of I: ",type(i))
+print("Type of J: ",type(j))
+print("Type of K: ",type(k))
+print("Type of L: ",type(l))
+print("Type of M: ",type(m))
+print("Type of N: ",type(n))
+print("Type of O: ",type(o))
+print("Type of P: ",type(p))
+print("Type of Q: ",type(q))
 
-if boolean is True:
-    print("the name has been changed")
-else:
-    print("the name has not been changed")
+# Type casting
 
+add = c + float(d)
+print("Addition of C and D: ",add)
+sub = c - float(e)
+print("Subtraction of C and E: ",sub)
+boolean = bool(m)
+print("Boolean of M: ",boolean)
+string_conversion = str(g)
+print("String conversion of G: ",type(string_conversion))
 
+# Time stamp 2 hours 20 minutes 12 seconds
