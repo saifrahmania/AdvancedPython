e8c7f85027c415fd057bf84992a558b1328b1613
saifrahmania <89687720+saifrahmania@users.noreply.github.com>
2024-04-23 08:03:03 +0600

    some prctice done on python

--- PythonEndToEndAapnaCollege/Ch1_VAriablesAndDatatypes/CSTutorial.py
+++ PythonEndToEndAapnaCollege/Ch1_VAriablesAndDatatypes/CSTutorial.py
@@ -1,3 +1,4 @@
+from math import floor
 a = 5
 b = 3
 sum = a+b
@@ -44,9 +45,82 @@ comments are written here আমি এখানে কমেন্ট লিখ
 # providing input 
 name = input("Enter your name: ")
 print("Your name is: ",name)
-age = int(input("Enter your age: "))
+age = float(input("Enter your age: "))
 print("Your age is: ",age)
 university = input("Enter your university name: ")
 print("Your university is: ",university)
 
 
+# if-elif-else
+
+if age > 18:
+    print("You are eligible for voting")
+elif age == 18:
+    print("You are eligible for voting but you are not an adult")
+else:  
+    print("You are not eligible for voting")
+light = input("Enter the color of the light: ")
+if light == "red":
+    print("Stop")
+elif light == "yellow":
+    print("Ready")
+else:
+    print("Go")
+
+# # 
+# # while loop
+
+i = 0
+while i < 5:
+    print("Value of i: ",i)
+    i += 1
+
+# # for loop
+
+for i in range(5):
+    print("Value of i: ",i)
+
+# # for loop with step
+
+for i in range(0,10,2):
+    print("Value of i: ",i)
+
+# # for loop with negative step
+
+for i in range(10,0,-2):
+    print("Value of i: ",i)
+
+# # for loop with negative step
+
+# problem
+A = int(input("Enter the value of A: "))
+G  = input("M/F : ")
+if((A == 1 or A == 2) and G == "M"):
+    print("fee is 10")
+elif((A == 3 or A == 4) and G == "F"):
+    print("fee is 200")
+elif(A == 5  and G == "M"):
+    print("fee is 300")
+else:
+    print("no fee")
+
+# time 1:39:42
+
+# single line conditional statement 
+
+food  = input(" Food : ")
+eat  = "Yes" if food  == "Cake" or "Cola" else "No"
+print(eat)
+
+# clevr if
+
+age = int(input("Enter your age: "))
+status = ("Adult","Child")[age < 18]
+print("You are a: ",status)
+
+# Calculate simple interest
+p = float(input("Enter the principal amount: "))
+r = float(input("Enter the rate of interest: "))
+t = float(input("Enter the time: "))
+si = p*r*t/100
+print("Simple interest: ",si)
