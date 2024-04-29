5ccd56275a37c86c0554de51b49da7daca7222e0
saifrahmania <89687720+saifrahmania@users.noreply.github.com>
2024-04-29 10:20:38 +0600

    dictionary have been practiced

---
+++ PythonEndToEndAapnaCollege/Ch4_DictionaryAndSet/lecture_four.py
@@ -0,0 +1,20 @@
+info = {
+    "name": "Aapna College",
+    "course": "Python",
+    "year": 2021,
+    "students": 1000,
+    "classRepresentative" : {
+        "name": "Shribastab",
+        "year": 2021,
+        "branch": "CSE"
+    },
+    "Owner" : "Hemant",
+    "location": "Jaipur",
+    "teachers": ["Hemant", "Shribastab", "Rahul"]
+}
+
+print(info["Owner"])
+print(info["teachers"])
+print(info["classRepresentative"])
+
+# time stamp 04:25:09
