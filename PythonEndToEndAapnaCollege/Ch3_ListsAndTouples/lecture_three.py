41f50d24796e8c44acd92304c87adfb885141625
saifrahmania <89687720+saifrahmania@users.noreply.github.com>
2024-08-01 22:29:21 +1000

    something changed

--- PythonEndToEndAapnaCollege/Ch3_ListsAndTouples/lecture_three.py
+++ PythonEndToEndAapnaCollege/Ch3_ListsAndTouples/lecture_three.py
@@ -21,21 +21,101 @@
 # List can be unpacked
 # List can be copied
 # List can be joined
-marks = [94.4, 87.5, 90.0, 78.9, 85.0]
-print(marks)
-print(type(marks))
-print(len(marks))
-marks.append(80.0)
-print(marks)
-marks.insert(2, 84.0)
-print(marks)
-marks.remove(90.0)
-print(marks)
-marks.pop()
-print(marks)
-marks.sort()
-print(marks)
-marks.sort(reverse=True)
-print(marks)
+# marks = [94.4, 87.5, 90.0, 78.9, 85.0]
+# print(marks)
+# print(type(marks))
+# print(len(marks))
+# marks.append(80.0)
+# print(marks)
+# marks.insert(2, 84.0)
+# print(marks)
+# marks.remove(90.0)
+# print(marks)
+# marks.pop()
+# print(marks)
+# marks.sort()
+# print(marks)
+# marks.sort(reverse=True)
+# print(marks)
+# print(marks.pop(2))
 
-# Completed 3:42:18
+# # Completed 3:42:18
+
+# # Strings
+
+# String is a sequence of characters
+# String is immutable
+# String is defined using single quotes ('') or double quotes ("")
+# String can be empty
+# String can be accessed using index
+# String can be sliced
+# String can be concatenated
+# String can be repeated
+# String can be iterated
+# String can be checked for membership
+# String can be formatted
+# String can be converted to uppercase
+# String can be converted to lowercase
+# String can be stripped
+# String can be split
+# String can be joined
+# name = "John Doe"
+# print(name)
+# print(type(name))
+# print(len(name))
+# print(name[0])
+# print(name[1:4])
+# print(name + " Smith")
+# print(name * 3)
+# for char in name:
+#     print(char)
+# print("Doe" in name)
+# print("Doe" not in name)
+# print("Hello, {}!".format(name))
+# print(name.upper())
+# print(name.lower())
+# print(name.strip())
+# print(name.split())
+# print("-".join(name.split()))
+# # Appending, Sorting, and Removing for Strings
+
+# # Appending to the string
+# name += " Smith"
+# print(name)
+
+# # Sorting the string in alphabetical order
+# sorted_name = "".join(sorted(name))
+# print(sorted_name)
+
+# # Removing a substring from the string
+# substring = "Doe"
+# name = name.replace(substring, "")
+# print(name)
+
+# Touples
+
+# Tuple is a collection of items in a particular order
+# Tuple is immutable
+# Tuple is defined by parentheses ()
+# Tuple can have any data type
+# Tuple can have any number of items
+# Tuple can have duplicate items
+# Tuple can have nested tuples
+# Tuple can have different data types
+# Tuple can be empty
+# Tuple can be accessed using index
+# Tuple can be sliced
+# Tuple can be concatenated
+# Tuple can be repeated
+# Tuple can be iterated
+# Tuple can be unpacked
+# Tuple can be copied
+# Tuple can be joined
+# 
+# Create a list of alphabets
+alphabets = ['b', 'a', 'd', 'c', 'e']
+
+# Sort the list
+alphabets.sort()
+
+print(alphabets)  # Output: ['a', 'b', 'c', 'd', 'e']
